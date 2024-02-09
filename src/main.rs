45f1d19d60c8243e709a21262cb2fe7b22a7c22a
barrett <onkoe@protonmail.com>
2024-02-09 12:15:55 -0600

    chore: Remove old labels

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,11 @@ use opencv::{
     imgcodecs::imread,
     imgproc::LINE_4,
 };
-use tracing::Level;
 
-#[rustfmt::skip] // keep this monstrosity outta my eyes
-const CLASSES_LABELS: [&str; 80] = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"];
+// FIXME(note): our current model ONLY has the orange mallet in its training data lol
+const CLASSES_LABELS: [&str; 1] = ["orange mallet"];
+const MODEL_NAME: &str = "yolov8_m";
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
