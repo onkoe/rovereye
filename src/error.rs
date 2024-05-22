14530606a92b148300bd60b07bcfd8817d1ac2d9
Barrett <onkoe@protonmail.com>
2024-05-21 23:07:05 -0500

    feat(ModelError): implement `From<Self>` for PyErr

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use pyo3::types::PyString;
 use thiserror::Error;
 
 /// An error that can occur when creating a new model.
@@ -11,3 +12,20 @@ pub enum ModelError {
     ModelPathDoesntExist(String),
 }
 
+impl From<ModelError> for pyo3::PyErr {
+    fn from(err: ModelError) -> Self {
+        match err {
+            ModelError::NoCaptureDevicePath(path) => Self::new::<PyString, _>(format!(
+                "The given capture device (camera) path doesn't exist: `{}`",
+                path
+            )),
+            ModelError::NoCaptureDeviceNumber(number) => Self::new::<PyString, _>(format!(
+                "The specified OpenCV capture device number has no assigned camera: `{}`",
+                number
+            )),
+            ModelError::ModelPathDoesntExist(path) => {
+                Self::new::<PyString, _>(format!("The given model path doesn't exist: `{}`", path))
+            }
+        }
+    }
+}
